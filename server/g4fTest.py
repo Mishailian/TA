edd8d22dfae70c0d36d5f9fedc68d4fb62e7b015
Mishailian <127056840+Mishailian@users.noreply.github.com>
2023-11-28 20:39:36 +0500

    Update g4fTest.py

--- server/g4fTest.py
+++ server/g4fTest.py
@@ -2,11 +2,12 @@ import g4f
 import time
 
 # НАСТРОЙКИ CHAT_GPT
-g4f.logging = True
-g4f.check_version = False
-time_start = time.time()
 
 def ask_gpt(promt) -> str:
+    g4f.logging = True
+    g4f.check_version = False
+    time_start = time.time()
+    
     response = g4f.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{
@@ -16,11 +17,10 @@ def ask_gpt(promt) -> str:
             ],
         stream=True,
     )
-
     ans_message = ''
     for message in response:
-        ans_message += message
-
+        ans_message += messag
+        
     return ans_message
 
 result = ask_gpt('hello')
