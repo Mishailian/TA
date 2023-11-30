44e59de53f8ff7e1af6fa44dd6e171a9121eaee2
FankiCH <avdeev.canek@yandex.ru>
2023-12-01 00:04:44 +0500

    Update user_interface.py

--- client/user_interface.py
+++ client/user_interface.py
@@ -38,10 +38,20 @@ def question(message, answer1, answer2, answer3, answer4, question1, choice, req
         keyboard1.row(button1, button2)
         bot.send_message(message.chat.id, question1, reply_markup=keyboard1)
     elif step == False and choice == True:
+        if message.text == n_data['questions'][2]['no'] or message.text == n_data['questions'][3]['yes']:
+            request.append("1")
+            request.append("2")
+            request.append("3")
+        elif  message.text == n_data['questions'][3]['no']:
+            request.append("2")
+            request.append("3")
+        else:
+            request.append("3")
         script(message)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    request.clear
     keyboard1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton(text=n_data['questions'][0]['inf'])
     keyboard1.row(button1)
@@ -55,13 +65,13 @@ def question1(message):
     step = False
     if message.text == n_data['questions'][0]['inf']:
         step = True
-        request.append('type_inf')
+        request.append('информационный')
     elif message.text == n_data['questions'][0]['edu']:
         step = True
-        request.append('type_questions')
+        request.append('образовательный')
     elif message.text == n_data['questions'][0]['play']:
         step = True
-        request.append('type_play')
+        request.append('развлекательный')
     if step:
         keyboard1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton(text=n_data['questions'][1]['shorts'])
@@ -74,14 +84,14 @@ def question1(message):
 def question2(message):
     step = False
     if message.text == n_data['questions'][1]['shorts']:
-        request.append('format_short')
+        request.append('короткое видео')
         step = True
     elif message.text == n_data['questions'][1]['video_blog']:
         step = True
-        request.append('format_video_blog')
+        request.append('video_blog')
     elif message.text == n_data['questions'][1]['youTube']:
         step = True
-        request.append('format_YouTube')
+        request.append('видео для YouTube')
     if step:
         keyboard1 = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         button1 = types.KeyboardButton(text=n_data['questions'][2]['yes'])
@@ -97,7 +107,7 @@ def question3(message):
 
 def question4(message):
     question(message, n_data['questions'][3]['yes'], n_data['questions'][3]['no'], n_data['questions'][4]['yes'],
-             n_data['questions'][4]['no'], n_data['questions'][4]['light'], True, 'get_instruction_frame')
+             n_data['questions'][4]['no'], n_data['questions'][4]['light'], True, 'Как правильно вести себя в кадре?')
     bot.register_next_step_handler(message, question5)
              
 
@@ -106,10 +116,11 @@ def question5(message):
     step = False
     if message.text == n_data['questions'][4]['yes']:
         bot.send_message(message.chat.id, 'Понял вас, свет не нужен')
+        request.append('5')
         script(message)
     elif message.text == n_data['questions'][4]['no']:
         bot.send_message(message.chat.id, 'Так и запишем!')
-        request.append('get_light')
+        request.append('Так же дай совет, как правильно настроить свет в кадре')
         script(message)
     
 
@@ -117,8 +128,9 @@ def question6(message):
     step = False
     if message.text == n_data['questions'][5]['yes']:
         step = True
-        request.append('get_script')
+        request.append('Дай подробный план для видео.')
     elif message.text == n_data['questions'][5]['no']:
+        request.append('6scrpt')
         step = True
     if step:
         keyboard1 = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -133,6 +145,7 @@ def question7(message):
         step = True
         request.append('get_music')
     elif message.text == n_data['questions'][6]['no']:
+        request.append('7mus')
         step = True
     if step:
         keyboard1 = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -148,6 +161,7 @@ def question8(message):
         bot.send_message(message.chat.id, n_data['questions'][8]['description_screensaver'])
         bot.register_next_step_handler(message, question9)
     elif message.text == n_data['questions'][7]['no']:
+        request.append('8zas')
         step = False
         bot.send_message(message.chat.id, n_data['questions'][9]['idea_video'])
         bot.register_next_step_handler(message, question10)
@@ -171,8 +185,10 @@ def question10(message):
     request.append(f"Идея видео: {message.text}")
     bot.send_message(message.chat.id, "Пожалуйста, подождите пару минут, бот обрабатывает все ваши запросы.")
     print('Запрос отправлен')
-    a = ask_gpt(str(request))
-    print(a)
+    print(request)
+    bot.send_message(message.chat.id, ask_gpt(str(request)))
+    request.clear
+
 
 if __name__ =="__main__":
     bot.infinity_polling()
